1985878426251ad844a1d007c0b796a99fe8f4d8
Kishor <kishor.muruganandham@fssa.freshworks.com>
2023-09-11 17:28:33 +0530

    Initial Commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,27 @@
+export default function AboutMe() {
+	return (
+		<section id="AboutMe" className="about--section">
+			<div className="about--section--img">
+				<img src="./img/about-me.jpg" alt="About Me" />
+			</div>
+			<div className="hero--section--content--box about--section--box">
+				<div className="hero--section--content">
+					<h1 className="skills-section--heading">About Me</h1>
+					<p className="hero--section-description">
+						Hi I am Kishor from Pollachi, Coimbatore. I have done my schooling in BVN MHSS, Pollachi and completed my diploma in Mechanical
+						Engineering at PSG Polytechnic College in 2022. My personal goal is to build my dream house and my professional goal is to become a
+						Software Engineer. From my childhood I have been very curious about computers and stuff related to technology, so I joined
+						Freshworks Software STS Academy as my first step towards my career. Now I became a web developer with skills in HTML, CSS,
+						JavaScript, and MySQL. As well as a growing expertise in Java programming, and created a library management system and a CSS
+						generators website using HTML,CSS and JAVA.
+					</p>
+					<p className="hero--section-description">
+						I am proficient in Adobe's creative suite, Google suite and Microsoft Office. I have also earned certifications from Hacker Rank in
+						problem-solving(Basic), CSS, SQL(Basic, Intermediate), JavaScript(Basic) and Java (Basic). I am passionate about driving and
+						listening to music. I use tools like Google Calendar, Notion, and Todoist to stay organized and be productive.
+					</p>
+				</div>
+			</div>
+		</section>
+	);
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Kishor</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description">
+          Welcome to My Portfolio
+            <br /> Building Awesome Web Experiences
+          </p>
+        </div>
+        <a href="./kishor-resume.pdf" download className="btn btn-primary">Download Resume</a>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.jpg" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
